877510f2faffafa9c9fe2ef0e08bc89113c19585
Sean Sullivan <seansulli@gmail.com>
2013-01-09 09:40:02 -0800

    Do not throw Exception in rawread due to long running consumers exiting when a new message is not received within timeout period

--- PhpAmqpLib/Wire/AMQPReader.php
+++ PhpAmqpLib/Wire/AMQPReader.php
@@ -54,12 +54,6 @@ class AMQPReader
             while ($read < $n && !feof($this->sock->real_sock()) &&
                     (false !== ($buf = fread($this->sock->real_sock(), $n - $read)))) {
 
-                // get status of socket to determine whether or not it has timed out
-                $info = stream_get_meta_data($this->sock->real_sock());
-                if($info['timed_out']) {
-                    throw new \Exception('Error reading data. Socket connection timed out');
-                }
-
                 $read += strlen($buf);
                 $res .= $buf;
             }
@@ -74,6 +68,7 @@ class AMQPReader
             if(strlen($this->str) < $n)
                 throw new \Exception("Error reading data. Requested $n bytes while string buffer has only " .
                                      strlen($this->str));
+
             $res = substr($this->str,0,$n);
             $this->str = substr($this->str,$n);
             $this->offset += $n;
